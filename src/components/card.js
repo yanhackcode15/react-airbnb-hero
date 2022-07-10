04bbb996a921310a6ac7b3926bb7c37d9a56376e
yanhackcode15 <iamyanhu@gmail.com>
2022-07-10 11:59:38 -0700

    use props and array mapping

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,22 @@
 import React from "react"
-import card1 from "../images/card 1.png"
-import card2 from "../images/card 2.png"
-import card3 from "../images/card 3.png"
 import star from "../images/star.png"
 import dot from "../images/ellipse.png"
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div>
-            <img className="card-image" src={card1} />
+            <img className="card-image" src={props.image} />
                     <div className="ratings">
                         <img className="star-image" src={star} />
-                        <span>(5) 6 </span>
+                        <span>({props.rating}) {props.reviewCount} </span>
                         <img className="dot" src={dot} />
-                        <span>USA</span>
+                        <span>{props.country}</span>
                     </div>
                     <div className="lesson-name">
-                        <span>Life lessons with Katie Zaferes</span>
+                        <span>{props.title}</span>
                     </div>
                     <div className="lesson-price">
-                        <span className="bold">From $136</span> / person
+                        <span className="bold">From ${props.price}</span> / person
                     </div>
         </div>
     )
--- src/components/experiences.js
+++ src/components/experiences.js
@@ -1,22 +1,32 @@
 import React from "react"
 import Card from "./card"
+import card1 from "../images/card 1.png"
+import CardData from "../../data"
 
 export default function Experiences() {
+    const mapper = function (card){
+        console.log(card.coverImg)
+        return (<li className="card">
+        <Card 
+        image={card.coverImg}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        country="USA"        
+        title={card.title}
+        price={card.price}
+        />
+        </li>)
+    }
+    const cards = CardData.map(mapper)
     return (
         <div className="experiences-text">
             <h1>Online Experiences</h1>
             <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
             </p>
             <ul className="cards">
-                <li className="card">
-                    <Card />
-                </li>
-                <li className="card">
-                    <Card />
-                </li>
-                <li className="card">
-                    <Card />
-                </li>
+                {
+                    cards
+                }
             </ul>
         </div>
     )
