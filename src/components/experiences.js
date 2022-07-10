2ed0b572dd2a00ade22b0c7b197c0d62d7bc7acd
yanhackcode15 <iamyanhu@gmail.com>
2022-07-10 12:51:58 -0700

    flex no wrap and overflown auto

--- src/components/experiences.js
+++ src/components/experiences.js
@@ -19,14 +19,14 @@ export default function Experiences() {
     }
     const cards = CardData.map(mapper)
     return (
-        <div className="experiences-text">
-            <h1>Online Experiences</h1>
-            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
-            </p>
+        <div>
+           <div className="experiences-text">
+                <h1>Online Experiences</h1>
+                <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+                </p>
+            </div>
             <ul className="cards">
-                {
-                    cards
-                }
+                {cards}              
             </ul>
         </div>
     )
